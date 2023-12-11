ae4ecf7012d53163a9a7637e95500479db765b46
halva2251 <znasku4@gmail.com>
2023-12-11 18:39:57 +0100

    updated urls and index file to refer to details page

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -17,12 +17,13 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from core.views import index, contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('items/', include('item.urls')),
     path('contact/', contact, name='contact'),
     path('', index, name='index'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
