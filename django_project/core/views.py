95721bab7535b484219ba9fc5bfd1d7371f737a1
halva2251 <znasku4@gmail.com>
2024-01-02 17:14:42 +0100

    added a view and html file for sign up page

--- django_project/core/views.py
+++ django_project/core/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 from item.models import Category, Item
-# Create your views here.
+
+from .forms import SignupForm
 
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
@@ -14,3 +15,10 @@ def index(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+def signup(request):
+    form = SignupForm()
+
+    return render(request, 'core/signup.html',{
+        'form': form
+    })
